09869a894caa80c2a6998a5fdc33d035ff7919cd
mentaho <bub.al@mail.ru>
2024-11-13 22:20:11 -0600

    DI, IoC, Beans, BeanFactory, ApplicationContext [#505289]

--- src/main/java/ru/job4j/bmb/DIByDirectInjectMain.java
+++ src/main/java/ru/job4j/bmb/DIByDirectInjectMain.java
@@ -1,7 +1,9 @@
 package ru.job4j.bmb;
+/**
+ *  закоментирован непонятный импорт import ru.job4j.bmb.model.Message;
+ */
 
 import ru.job4j.bmb.content.Content;
-//import ru.job4j.bmb.model.Message;
 import ru.job4j.bmb.services.BotCommandHandler;
 import ru.job4j.bmb.services.TelegramBotService;
 
