72b0a2e2ead4461cc747b06c8d3f0c0696a86551
mentaho <bub.al@mail.ru>
2024-11-13 22:07:24 -0600

    DI, IoC, Beans, BeanFactory, ApplicationContext [#505289]

--- src/main/java/ru/job4j/bmb/services/BotCommandHandler.java
+++ src/main/java/ru/job4j/bmb/services/BotCommandHandler.java
@@ -4,8 +4,7 @@ import org.springframework.stereotype.Service;
 import ru.job4j.bmb.content.Content;
 
 @Service
-public class BotCommandHandler
- {
+public class BotCommandHandler {
     void receive(Content content) {
         System.out.println(content);
     }
