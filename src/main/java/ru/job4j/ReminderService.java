1fde98b0c8efd736b2cdd9b89808feaceaeca72f
mentaho <bub.al@mail.ru>
2024-11-17 20:53:11 -0600

    Жизненный цикл Spring Bean [#505292]

--- src/main/java/ru/job4j/ReminderService.java
+++ src/main/java/ru/job4j/ReminderService.java
@@ -1,4 +1,17 @@
 package ru.job4j;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class ReminderService {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Bean is going through @PostConstruct init");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Bean will be destroyed via @PreDestroy.");
+    }
 }
--- src/main/java/ru/job4j/bmb/services/TelegramBotService.java
+++ src/main/java/ru/job4j/bmb/services/TelegramBotService.java
@@ -1,10 +1,18 @@
 package ru.job4j.bmb.services;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Service;
 import ru.job4j.bmb.content.Content;
 
 @Service
 public class TelegramBotService {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Bean is going through @PostConstruct init");
+    }
+
     private final BotCommandHandler handler;
 
     public TelegramBotService(BotCommandHandler handler) {
@@ -13,5 +21,10 @@ public class TelegramBotService {
 
     public void content(Content content) {
         handler.receive(content);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Bean will be destroyed via @PreDestroy.");
     }
 }
